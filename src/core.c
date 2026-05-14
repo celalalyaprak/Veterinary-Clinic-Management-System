74fb4c715408722b45be496d49af5494043d12ed
CELAL ALYAPRAK <celalalyaprak@gmail.com>
2026-05-14 18:23:31 -0400

    Update core.c

--- src/core.c
+++ src/core.c
@@ -1,23 +1,3 @@
-/*/////////////////////////////////////////////////////////////////////////
-                      Veterinary-Clinic-Management-System
-Author     : Guan-Wei Huang
-Email      : gwhuang24@gmail.com
-
-Copyright 2024 Guan-Wei Huang
-All rights reserved.
-
-This project, including all source code and associated documentation files, 
-is the intellectual property of Lynn Lin. Unauthorized copying, distribution, 
-modification, or use of any part of this software, in whole or in part, without 
-explicit written permission from the copyright owner is strictly prohibited.
-
-This software is provided "as is" without warranty of any kind, express or implied, 
-including but not limited to the warranties of merchantability, fitness for a 
-particular purpose, and noninfringement. In no event shall the copyright holder be 
-liable for any claim, damages, or other liability, whether in an action of contract, 
-tort, or otherwise, arising from, out of, or in connection with the software or the 
-use or other dealings in the software.
-/////////////////////////////////////////////////////////////////////////*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include "core.h"
